6065b84a79abff2751b22df47a94ace215b043d9
kuro177013 <kurozz.toki@gmail.com>
2024-11-03 13:38:55 +0800

    Add files via upload
    first commit

---
+++ js/mainJS.js
@@ -0,0 +1,44 @@
+
+// debugging
+// alert("Test");
+
+// console.log("Hello world!");
+
+
+
+/*********************************************************************************************************
+ * 
+ * 
+ *  PLAYER 1 FUNCTION
+ * 
+ * 
+ *********************************************************************************************************/
+let PlayerCounter_1 = 1;
+
+function playerHit_1() {
+    // let counter = 1;
+
+    PlayerCounter_1++;
+
+    console.log(PlayerCounter_1);
+
+    document.getElementById("displayPLayerCounter1").innerHTML = PlayerCounter_1;
+}
+/*********************************************************************************************************
+ * 
+ * 
+ *  PLAYER 2 FUNCTION
+ * 
+ * 
+ *********************************************************************************************************/
+let PlayerCounter_2 = 1;
+
+function playerHit_2() {
+    // let counter = 1;
+
+    PlayerCounter_2++;
+
+    console.log(PlayerCounter_2);
+
+    document.getElementById("displayPLayerCounter2").innerHTML = PlayerCounter_2;
+}
